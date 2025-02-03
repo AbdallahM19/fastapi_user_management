c84910699dbca9327a83ac058ab875172c30b5c6
AbdallahM19 <abdullahmaabakar@gmail.com>
2025-02-03 03:04:25 +0200

    Integrate user API router into FastAPI application

--- app/__init__.py
+++ app/__init__.py
@@ -2,8 +2,11 @@
 
 from fastapi import FastAPI
 from app.database import create_db_and_tables
+from app.user_api import user_apis
 
 app = FastAPI()
+
+app.include_router(user_apis, tags=["users-api"])
 
 
 @app.on_event("startup")
