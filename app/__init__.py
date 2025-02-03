155a7c4c09c193291ef2425cabfd8fa25d687011
AbdallahM19 <abdullahmaabakar@gmail.com>
2025-02-03 03:18:09 +0200

    Integrate user management API router into FastAPI application

--- app/__init__.py
+++ app/__init__.py
@@ -3,10 +3,12 @@
 from fastapi import FastAPI
 from app.database import create_db_and_tables
 from app.user_api import user_apis
+from app.user_management_api import user_management_apis
 
 app = FastAPI()
 
 app.include_router(user_apis, tags=["users-api"])
+app.include_router(user_management_apis, tags=["user-management-api"])
 
 
 @app.on_event("startup")
