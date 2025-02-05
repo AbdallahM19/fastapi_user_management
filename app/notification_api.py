e74191e6d7030b5b955e9bdce39326521e0ddab6
AbdallahM19 <abdullahmaabakar@gmail.com>
2025-02-05 11:40:21 +0200

    Refactor notification API to include response handling and prepare for receive notification functionality

--- app/notification_api.py
+++ app/notification_api.py
@@ -1,10 +1,10 @@
 """/app/notification_api.py"""
 
-from typing import Annotated, Optional
+from typing import Annotated, Union, Optional
 
-from fastapi import APIRouter, BackgroundTasks
-from app.helper import EMAIL_REGEX, user_helper, res_helper, notification_helper
-
+from fastapi import APIRouter, BackgroundTasks, Response
+from app.helper import SessionDep, user_helper, res_helper, notification_helper
+from app.database import NotificationBase, Notification
 
 notification_apis = APIRouter(
     tags=["notification-api"],
@@ -29,3 +29,16 @@ async def send_notification(
     """Send notification"""
     background_tasks.add_task(notification_helper.send_notification_task, email, message="some notification")
     return {"message": "Notification sent"}
+
+
+# @notification_apis.get("/receive-notification/{email}")
+# async def receive_notification(
+#     background_tasks: BackgroundTasks,
+#     email: Optional[str] = None,
+# ):
+#     """Receive notification"""
+#     if not user_helper.check_email_match(email):
+#         return {"message": "Invalid email"}
+
+#     background_tasks.add_task(notification_helper.receive_notification_task, email)
+#     return {"message": "Notification received"}
