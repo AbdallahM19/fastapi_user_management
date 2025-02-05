3f65b1a241cb3c0380b04cae2f59b4635bc0129e
AbdallahM19 <abdullahmaabakar@gmail.com>
2025-02-05 04:12:34 +0200

    Add notification API router to the FastAPI application

--- app/__init__.py
+++ app/__init__.py
@@ -4,11 +4,13 @@ from fastapi import FastAPI
 from app.database import create_db_and_tables
 from app.user_api import user_apis
 from app.user_management_api import user_management_apis
+from app.notification_api import notification_apis
 
 app = FastAPI()
 
 app.include_router(user_apis)
 app.include_router(user_management_apis)
+app.include_router(notification_apis)
 
 
 @app.on_event("startup")
