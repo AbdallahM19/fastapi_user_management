3fc4064c549869fd134acb4fb6f4a63cbd717244
AbdallahM19 <abdullahmaabakar@gmail.com>
2025-02-06 02:45:01 +0200

    Enhance FastAPI initialization with metadata, including title, description, version, terms of service, license info, and OpenAPI tags for improved API documentation

--- app/__init__.py
+++ app/__init__.py
@@ -6,7 +6,30 @@ from app.user_api import user_apis
 from app.user_management_api import user_management_apis
 from app.notification_api import notification_apis
 
-app = FastAPI()
+app = FastAPI(
+    title="FastAPI User Management",
+    description="This is a simple user management system built with FastAPI",
+    version="0.1.0",
+    terms_of_service="https://www.example.com/terms/",
+    license_info={
+        "name": "Apache 2.0",
+        "identifier": "MIT",
+    },
+    openapi_tags=[
+        {
+            "name": "users-api",
+            "description": "User API: Create, read, update, and delete users",
+        },
+        {
+            "name": "user-management-api",
+            "description": "User Management API: Login, register, and manage users",
+        },
+        {
+            "name": "notification-api",
+            "description": "Notification API: Send and receive notifications",
+        },
+    ]
+)
 
 app.include_router(user_apis)
 app.include_router(user_management_apis)
