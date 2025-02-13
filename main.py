c755612aaf7f184f0c809527e7b7e1d918e2fe39
AbdallahM19 <abdullahmaabakar@gmail.com>
2025-02-13 10:20:44 +0200

    Enable auto-reload for development server in main.py

--- main.py
+++ main.py
@@ -4,4 +4,4 @@ from app import app
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=5000)
+    uvicorn.run("main:app", host="0.0.0.0", port=5000, reload=True)
